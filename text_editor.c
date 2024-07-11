e406aa63986c6292586e28cea2dde214bc8ddad5
vic-bart <vicbart08@gmail.com>
2024-07-11 11:51:30 +1000

    Printing to terminal

--- text_editor.c
+++ text_editor.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+// Tutorial recommends the unistd library, but that only works on Linux OSs; since I'm running this on a Windows OS, I've seen suggestions to use the io library instead (the alternatives seem convoluted, or require a virtual Linux environment, which is not what I want; I want this to run on Windows).
+// stdin (https://en.cppreference.com/w/c/io)
+// fread (https://en.cppreference.com/w/c/io/fread)
 
 int main() {
-  printf("Hello");
+  char c;
+  while (fread(&c, 1, 1, stdin) == 1 && c != 'q'){
+    fwrite(&c, 1, sizeof(c), stdout);
+  };
   return 0;
 }
